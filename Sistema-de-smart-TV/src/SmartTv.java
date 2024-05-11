e88d35024bbf80df11382afd9d0dc7d59e6c503b
Williansilva2207 <williansilva.drigue@gmail.com>
2024-05-11 12:35:22 -0300

    SmartTv atualizada

--- Sistema-de-smart-TV/src/SmartTv.java
+++ Sistema-de-smart-TV/src/SmartTv.java
@@ -2,4 +2,30 @@ public class SmartTv {
     boolean ligado = false;
     int canal = 13;
     int volume = 100;
+
+    public void ligar(){
+        ligado = true;
+    }
+
+    public void desligar(){
+        ligado = false;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+    }
+    
+    public void diminuirVolume(){
+        volume--;
+    }
+
 }
+
--- Sistema-de-smart-TV/src/Usuario.java
+++ Sistema-de-smart-TV/src/Usuario.java
@@ -5,5 +5,20 @@ public class Usuario {
         System.out.println(smartTv.ligado);
         System.out.println(smartTv.canal);
         System.out.println(smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println(smartTv.ligado);
+
+        smartTv.aumentarCanal();
+        System.out.println(smartTv.canal);
+        
+        smartTv.diminuirCanal();
+        smartTv.diminuirCanal();
+        System.out.println(smartTv.canal);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        System.out.println(smartTv.volume);
     }
 }
