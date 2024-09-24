b9e37d43ba08a2646eb942ac1333cd75e589fc39
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2024-09-24 15:03:21 +0000

    botao

--- EXTRA/GUI/src/MinhaJanela.java
+++ EXTRA/GUI/src/MinhaJanela.java
@@ -1,10 +1,9 @@
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
 public class MinhaJanela extends JFrame{
     public MinhaJanela(){
         setLayout(new FlowLayout(FlowLayout.CENTER,10,20));
